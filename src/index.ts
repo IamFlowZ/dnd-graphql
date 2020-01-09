2d351182e262dcbfe969801e9cffb57a6b6ed67f
IamFlowZ <dakota.lewallen17@gmail.com>
2020-01-09 17:34:05 -0500

    setting up neo4j

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer, makeExecutableSchema } from 'apollo-server'
+import {augmentSchema} from 'neo4j-graphql-js'
+import neo4j from "neo4j-driver"
 
 import Schema from './schema/schema'
 const mySchema = new Schema()
 const typeDefs = mySchema.typeDefs
 const resolvers = mySchema.resolvers
+const schema = makeExecutableSchema({
+    typeDefs, resolvers
+})
+const modifiedSchema = augmentSchema(schema)
 
-const server = new ApolloServer({typeDefs, resolvers, tracing: true})
+const driver = neo4j.driver(
+    'bolt://localhost:7687',
+    neo4j.auth.basic('neo4j', 'letmein')
+)
+
+const server = new ApolloServer({typeDefs, resolvers, tracing: true, context: {driver}})
 server.listen().then(({url}) => 
     console.log(`Listening on: ${url}`)
 )
