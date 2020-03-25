f80efb75d655d520a69adc1cb0c2465c552a9d62
IamFlowZ <dakota.lewallen17@gmail.com>
2020-03-25 17:28:09 -0400

    weapon properties

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -2,9 +2,9 @@ import conditionSeed from "./conditions.seed";
 import abilityAndSkills from "./abilityScoreAndSkills.seed";
 import langsAndRaces from "./languagesAndRaces.seed";
 import classes from "./class.seed";
+import weaponProperties from "./weaponProperties.seed";
 
 export default function() {
-  classes();
   console.log(
     "call the functions of the information you would like to seed here.",
     __filename
