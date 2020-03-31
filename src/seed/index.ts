387751bd1ff0c403c70045faea8c3ee25c1546f7
IamFlowZ <dakota.lewallen17@gmail.com>
2020-03-31 06:43:00 -0400

    proficencies

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -4,8 +4,10 @@ import langsAndRaces from "./languagesAndRaces.seed";
 import classes from "./class.seed";
 import weaponProperties from "./weaponProperties.seed";
 import dmgTypes from "./damageTypes.seed";
+import profs from "./proficencies.seed";
 
 export default function() {
+  profs();
   console.log(
     "call the functions of the information you would like to seed here.",
     __filename
