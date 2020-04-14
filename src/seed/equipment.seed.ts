4f41a54a8278c0e000fd8321cb0ea5225f7d6a96
iamflowz <dakota.lewallen17@gmail.com>
2020-04-14 10:08:46 -0400

    starting equipment

---
+++ src/seed/equipment.seed.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+
+import neo4j from "neo4j-driver";
+
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic("neo4j", "letmein")
+);
+
+const categories = JSON.parse(
+  fs
+    .readFileSync(path.join(__dirname, "./sources/EquipmentCategories.json"))
+    .toString()
+);
+const equipment = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "./sources/Equipment.json")).toString()
+);
+
+export default function () {}
