10825c96337ff83dbb24b32d9f2bb46f5e058a7c
iamflowz <dakota.lewallen17@gmail.com>
2020-04-14 08:28:34 -0400

    stuff

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -1,13 +1,23 @@
 import conditionSeed from "./conditions.seed";
 import abilityAndSkills from "./abilityScoreAndSkills.seed";
 import langsAndRaces from "./languagesAndRaces.seed";
+import subrace from "./subraces.seed";
 import classes from "./class.seed";
+import subclass from "./subclass.seed";
 import weaponProperties from "./weaponProperties.seed";
 import dmgTypes from "./damageTypes.seed";
 import profs from "./proficencies.seed";
 
-export default function() {
-  profs();
+export default function () {
+  // conditionSeed();
+  // abilityAndSkills();
+  // langsAndRaces();
+  // classes();
+  // weaponProperties();
+  // dmgTypes();
+  // profs();
+  // subclass();
+  // subrace();
   console.log(
     "call the functions of the information you would like to seed here.",
     __filename
--- src/seed/proficencies.seed.ts
+++ src/seed/proficencies.seed.ts
@@ -8,14 +8,14 @@ const driver = neo4j.driver(
   neo4j.auth.basic("neo4j", "letmein")
 );
 
-const proficencies = JSON.parse(
+const proficiencies = JSON.parse(
   fs
     .readFileSync(path.join(__dirname, "../sources/Proficiencies.json"))
     .toString()
 );
 
 const createProf = `CREATE (a:Proficiency{name:$name, type:$type})`;
-const relateProf = names => `
+const relateProf = (names) => `
 MATCH (a:Proficiency{name:$name})
 MATCH (b)
 WHERE b.name IN [${names}]
@@ -23,15 +23,15 @@ WITH b, CASE WHEN ['Race', 'Class'] IN LABELS(b)
 CREATE (b) - [:HAS_PROF] -> (a)
 `;
 
-export default function() {
-  const createProfs = proficencies.map(prof => {
+export default function () {
+  const createProfs = proficiencies.map((prof) => {
     if (!(prof.type === "Skills" || prof.type === "Saving Throws")) {
       const session = driver.session();
       return session.run(createProf, { ...prof });
     }
   });
-  //   Promise.all(createProfs).catch(err => console.error(err));
-  const relateProfs = proficencies.map(prof => {
+  Promise.all(createProfs).catch((err) => console.error(err));
+  const relateProfs = proficiencies.map((prof) => {
     if (!(prof.type === "Skills" || prof.type === "Saving Throws")) {
       const classes = prof.classes.reduce((accu, curr, i) => {
         if (i === 0) {
@@ -52,7 +52,7 @@ export default function() {
         const session = driver.session();
         console.log(relateProf(`${classes}, ${races}`));
         return session.run(relateProf(`${classes}, ${races}`), {
-          name: prof.name
+          name: prof.name,
         });
       } else if (classes.length) {
         const session = driver.session();
@@ -63,5 +63,5 @@ export default function() {
       }
     }
   });
-  Promise.all(relateProfs).catch(err => console.error(err));
+  // Promise.all(relateProfs).catch(err => console.error(err));
 }
